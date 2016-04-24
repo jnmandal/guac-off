e3228461c5e8c70772e7a92ec010edf730aab2a3
John Mandalakas <john@mandalakas.com>
2016-04-24 16:23:21 -0500

    Fix the foreign key name for ratings

--- models/rating.js
+++ models/rating.js
@@ -5,7 +5,7 @@ module.exports = function(sequelize, DataTypes) {
     comments: DataTypes.TEXT,
     headline: DataTypes.STRING,
     user_id: DataTypes.INTEGER,
-    guacamole_id: DataTypes.INTEGER
+    ratable_id: DataTypes.INTEGER
   }, {
     underscored: true,
     classMethods: {
