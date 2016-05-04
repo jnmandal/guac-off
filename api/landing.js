be5d8d16fc84d1fb2f7a6f12a122ceef09f4f585
John Mandalakas <john@mandalakas.com>
2016-05-03 22:36:59 -0500

    Stub a results view

--- api/landing.js
+++ api/landing.js
@@ -3,6 +3,11 @@
 module.exports = function (app) {
   app.get('/', function (req, res) {
     res.set('Content-Type', 'text/html');
-    res.sendfile('./index.html');
+    res.sendfile('./views/index.html');
+  });
+
+  app.get('/results', function (req, res) {
+    res.set('Content-Type', 'text/html');
+    res.sendfile('./views/results.html');
   });
 }
