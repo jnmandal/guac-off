4077429f8ea2fdabfcf7baf7b30be1ab2152f517
John Mandalakas <john@mandalakas.com>
2016-05-09 21:10:40 -0500

    Set results to main landing page

--- api/landing.js
+++ api/landing.js
@@ -1,17 +1,17 @@
 'use strict';
 
 module.exports = function (app) {
+  // app.get('/', function (req, res) {
+  //   res.set('Content-Type', 'text/html');
+  //   res.render('index', {email: null});
+  // });
+  //
+  // app.get('/raters/:email', function(req, res) {
+  //   res.set('Content-Type', 'text/html');
+  //   res.render('index', {email:  req.params.email});
+  // })
+
   app.get('/', function (req, res) {
-    res.set('Content-Type', 'text/html');
-    res.render('index', {email: null});
-  });
-
-  app.get('/raters/:email', function(req, res) {
-    res.set('Content-Type', 'text/html');
-    res.render('index', {email:  req.params.email});
-  })
-
-  app.get('/results', function (req, res) {
     res.set('Content-Type', 'text/html');
     res.sendfile('./views/results.html');
   });
